4726c20c11022cc55ae6c7a11055d1ca34d18c1e
artemii <kavrese@gmail.com>
2025-07-23 03:05:46 +0300

    fixed televerse_plus.dart

--- lib/televerse_plus.dart
+++ lib/televerse_plus.dart
@@ -2,4 +2,4 @@ library;
 
 export 'package:televerse_plus/src/callbacks/callbacks.dart';
 export 'package:televerse_plus/src/handlers/handlers.dart';
-export 'package:televerse_plus/src/bot/bot_plus.dart';
+export 'package:televerse_plus/src/bot/bot.dart';
